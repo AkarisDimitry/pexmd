ab5f2b75ba8f6c57a99d576062fb694918154b7d
flopezar <flopezar@iar.unlp.edu.ar>
2018-03-07 17:19:49 -0300

    Agrego integrador de Andersen, en clase NVT.

--- pexmd/integrator/Integrator.py
+++ pexmd/integrator/Integrator.py
@@ -41,6 +41,30 @@ class NVT(Integrator):
   """
   NVT base class
   """
+
   def __init__(self, dt, temperature):
     self.temperature = temperature
     super().__init__(dt)
+
+class Andersen(NVT):
+  """
+  Andersen Thermostat
+  """
+
+  def __init__(self, dt, temperature, freq):
+    self.freq = freq
+    super().__init__(dt, temperature)
+
+  def first_step(self, x, v, a):
+    x = x + v*self.dt + 0.5*a*self.dt**2
+    v = v + 0.5*a*self.dt
+    return x, v
+
+  def last_step(self, x, v, a):
+    v = v + 0.5*a*self.dt
+    p_i = np.random.rand()
+    nparticles = len(x)
+    if p_i < self.freq * self.dt:
+        i_ran = np.random.randint(0,nparticles)
+        v [i_ran, :] = np.random.normal(0.,math.sqrt(self.temperature),size = 3)
+    return x, v
